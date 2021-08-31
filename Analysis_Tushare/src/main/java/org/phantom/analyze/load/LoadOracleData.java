b8fff67b850ec10235f7d84c3e043c9df2f190d4
phantom-zsq <1131558536@qq.com>
2021-08-31 22:32:27 +0800

    refactor the code

--- Analysis_Tushare/src/main/java/org/phantom/analyze/load/LoadOracleData.java
+++ Analysis_Tushare/src/main/java/org/phantom/analyze/load/LoadOracleData.java
@@ -18,23 +18,18 @@ public class LoadOracleData {
     }
 
     public void load() throws Exception {
-
         // A股复权行情
         Dataset<Row> proBar = session.read().jdbc(properties.getProperty("url"), "(select ts_code,date_format(trade_date,'%Y-%m-%d') as trade_date,close from pro_bar where ts_code=\'603997.SH\') tt", properties);
         proBar.createOrReplaceTempView("pro_bar");
-
         // 沪深股通成分股
         Dataset<Row> hkHoldDS = session.read().jdbc(properties.getProperty("url"), "(select * from hk_hold) tt", properties);
         hkHoldDS.createOrReplaceTempView("hs_const");
-
         // 沪深港通资金流向
         Dataset<Row> moneyflowHsgt = session.read().jdbc(properties.getProperty("url"), "(select * from moneyflow_hsgt) tt", properties);
         moneyflowHsgt.createOrReplaceTempView("moneyflow_hsgt");
-
         // 沪深股通十大成交股
         Dataset<Row> hsgtTop10 = session.read().jdbc(properties.getProperty("url"), "(select * from hsgt_top10) tt", properties);
         hsgtTop10.createOrReplaceTempView("hsgt_top10");
-
         // 沪深港股通成交明细
         Dataset<Row> hkHold = session.read().jdbc(properties.getProperty("url"), "(select ts_code,date_format(trade_date,'%Y-%m-%d') as trade_date,ratio from hk_hold where ts_code=\'603997.SH\') tt", properties);
         hkHold.createOrReplaceTempView("hk_hold");
