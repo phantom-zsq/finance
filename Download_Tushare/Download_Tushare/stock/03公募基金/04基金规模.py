3ddbf8be7f546098d3ea9c47789a2b834f3032ba
phantom-zsq <1131558536@qq.com>
2021-10-23 14:04:24 +0800

    init

---
+++ Download_Tushare/Download_Tushare/stock/03公募基金/04基金规模.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import tushare as ts
+import pandas as pd
+import time
+from sqlalchemy import create_engine
+
+# create mysql engine
+engine = create_engine('mysql+pymysql://root:123456@localhost:3306/tushare')
+# set Download_Tushare token
+ts.set_token('594b808743cb001d120038c81f3ec360ed7f6c81b45834d04a84a130')
+# get Download_Tushare api
+pro = ts.pro_api()
+
+# -------------------common start-------------------
+
+df = pro.fund_share(ts_code='150018.SZ')
+
+# -------------------common end-------------------
+# load to mysql
+res = df.to_sql('fund_share', engine, index=False, if_exists='append', chunksize=10000)
+print(res)
