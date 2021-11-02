186c98ae7f8908690d1e20e11bf8d398953ebd7a
phantom-zsq <1131558536@qq.com>
2021-11-03 00:34:18 +0800

    update read oracle data

--- Analysis_Tushare/src/main/java/org/phantom/analyze/load/LoadOracleData.java
+++ Analysis_Tushare/src/main/java/org/phantom/analyze/load/LoadOracleData.java
@@ -17,20 +17,23 @@ public class LoadOracleData {
     }
 
     public void load() throws Exception {
-        // A股复权行情
+        // A股截止到20201231的4265支股票列表
+        Dataset<Row> stockBasic = session.read().jdbc(properties.getProperty("url"), "(select * from stock_basic where list_date<'20210101' and market is not null) tt", properties);
+        stockBasic.createOrReplaceTempView("stock_basic");
+        // A股截止到20201231的4265支股票复权行情
         Dataset<Row> proBar = session.read().jdbc(properties.getProperty("url"), "(select * from pro_bar) tt", properties);
         proBar.createOrReplaceTempView("pro_bar");
-        // 沪深股通成分股
-        Dataset<Row> hkHoldDS = session.read().jdbc(properties.getProperty("url"), "(select * from hk_hold) tt", properties);
+        // A股截止到20201231的2319支沪深股通成分股
+        Dataset<Row> hkHoldDS = session.read().jdbc(properties.getProperty("url"), "(select * from hk_hold_statis where ts_code!='000043.SZ' and (ts_code like '%SH' or ts_code like '%SZ')) tt", properties);
         hkHoldDS.createOrReplaceTempView("hs_const");
         // 沪深港通资金流向
-        Dataset<Row> moneyflowHsgt = session.read().jdbc(properties.getProperty("url"), "(select * from moneyflow_hsgt) tt", properties);
+        Dataset<Row> moneyflowHsgt = session.read().jdbc(properties.getProperty("url"), "(select * from moneyflow_hsgt where trade_date>='20160629') tt", properties);
         moneyflowHsgt.createOrReplaceTempView("moneyflow_hsgt");
         // 沪深股通十大成交股
-        Dataset<Row> hsgtTop10 = session.read().jdbc(properties.getProperty("url"), "(select * from hsgt_top10) tt", properties);
+        Dataset<Row> hsgtTop10 = session.read().jdbc(properties.getProperty("url"), "(select * from hsgt_top10 where trade_date>='20160629') tt", properties);
         hsgtTop10.createOrReplaceTempView("hsgt_top10");
         // 沪深港股通成交明细
-        Dataset<Row> hkHold = session.read().jdbc(properties.getProperty("url"), "(select * from hk_hold) tt", properties);
+        Dataset<Row> hkHold = session.read().jdbc(properties.getProperty("url"), "(select * from hk_hold where ts_code!='000043.SZ' and exchange in('SH','SZ')) tt", properties);
         hkHold.createOrReplaceTempView("hk_hold");
         // 可转债基本信息
         Dataset<Row> cbBasic = session.read().jdbc(properties.getProperty("url"), "(select * from cb_basic) tt", properties);
