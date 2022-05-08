c2e666ed94735e81b1964c63caa330de0cc2bca9
phantom-zsq <1131558536@qq.com>
2022-05-08 22:29:43 +0800

    refactor

--- Analysis_Tushare/src/main/java/org/phantom/analyze/statistics/AmountOfBond.java
+++ Analysis_Tushare/src/main/java/org/phantom/analyze/statistics/AmountOfBond.java
@@ -25,22 +25,32 @@ public class AmountOfBond {
         new LoadOracleData().load();
         List<Row> rows = session.sql("select a.ts_code,a.trade_date,a.open,a.high,a.low,a.close,a.amount * 10000 as amount,b.issue_size * 100000000 as issue_size from cb_daily a inner join cb_issue b on a.ts_code=b.ts_code where a.open > 0 order by a.ts_code,a.trade_date").collectAsList();
         // issue size strategy
+        //issueSize(rows);
+        // moving average strategy
+        movingAverage(rows);
+        // exponential function strategy
+        exponentialFunction(rows);
+        // increase strategy
+        increase(rows);
+        // hong san bing strategy
+        hongSanBing(rows);
+    }
+
+    private void issueSize(List<Row> rows) throws Exception {
         for(int times = 1; times < 11; times++){
             for (int i = 0; i < 1; i++) {
                 for (int j = 1; j < 11; j++) {
                     for (int m = 1; m < 21; m++) {
                         for (int n = m; n < 21; n++) {
-                            issueSize(rows, times, i, j, m, n);
+                            issueSizeDetail(rows, times, i, j, m, n);
                         }
                     }
                 }
             }
         }
-        // moving average strategy
-        movingAverage(rows);
     }
 
-    private void issueSize(List<Row> rows, int times, int interval, int conditionOfBuy, int startofBuy, int endOfBuy) throws Exception {
+    private void issueSizeDetail(List<Row> rows, int times, int interval, int conditionOfBuy, int startofBuy, int endOfBuy) throws Exception {
         String last_ts_code = "";
         int greaterCount = 0;
         int lessCount = 0;
@@ -143,6 +153,68 @@ public class AmountOfBond {
     }
 
     private void movingAverage(List<Row> rows) throws Exception {
+
+    }
+
+    private void exponentialFunction(List<Row> rows) throws Exception {
+        Map<String, List<Row>> map = new HashMap<String, List<Row>>();
+        for(Row row : rows){
+            int i = 0;
+            String ts_code = row.getString(i++);
+            if(map.containsKey(ts_code)){
+                map.get(ts_code).add(row);
+            }else{
+                List<Row> list = new ArrayList<Row>();
+                list.add(row);
+                map.put(ts_code,list);
+            }
+        }
+        for(String ts_code : map.keySet()){
+            List<Row> list = map.get(ts_code);
+            for(int m=0; m<list.size(); m++){
+                Row row = list.get(m);
+                int i = 1;
+                String trade_date = row.getString(i++);
+                Double open = row.getDouble(i++);
+                Double high = row.getDouble(i++);
+                Double low = row.getDouble(i++);
+                Double close = row.getDouble(i++);
+                Double amount = row.getDouble(i++);
+                Double issue_size = row.getDouble(i++);
+                for(int n=m+1; n<list.size(); n++){
+                    Row row_new = list.get(n);
+                    int j = 1;
+                    String trade_date_new = row_new.getString(j++);
+                    Double open_new = row_new.getDouble(j++);
+                    Double high_new = row_new.getDouble(j++);
+                    Double low_new = row_new.getDouble(j++);
+                    Double close_new = row_new.getDouble(j++);
+                    Double amount_new = row_new.getDouble(j++);
+                    Double issue_size_new = row_new.getDouble(j++);
+                    if(amount_new >= amount * 2){
+                        amount = amount_new;
+                        if(n == list.size()-1){
+                            System.out.println(ts_code+": "+(n+1-m)+": "+trade_date+": "+trade_date_new);
+                            m = n;
+                        }
+                        continue;
+                    }else{
+                        if(n-m > 1){
+                            System.out.println(ts_code+": "+(n-m)+": "+trade_date+": "+trade_date_new);
+                            m = n - 1;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void increase(List<Row> rows) throws Exception {
+
+    }
+
+    private void hongSanBing(List<Row> rows) throws Exception {
 
     }
 }
