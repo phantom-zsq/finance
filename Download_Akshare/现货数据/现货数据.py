a68ce4ee48aa6b2c198a0f0de4957a7267363faf
phantom-zsq <1131558536@qq.com>
2025-10-12 16:56:12 +0800

    init

--- Download_Akshare/现货数据/现货数据.py
+++ Download_Akshare/现货数据/现货数据.py
@@ -1,20 +1,5 @@
-import requests
-from urllib3.exceptions import InsecureRequestWarning, SSLError
+import spot_price_qh_changed as ak
 
-try:
-    response = requests.get('https://www.99qh.com', verify=False)
-    response.raise_for_status()
-except SSLError as e:
-    print(f"遇到SSL错误: {e}")
-except InsecureRequestWarning:
-    print("存在不安全的请求警告，可能是证书验证问题。")
-except requests.HTTPError as http_err:
-    print(f"发生HTTP错误: {http_err}")
-else:
-    print("请求成功，证书验证通过。")
-    # 查看证书信息（如果请求成功）
-    cert = response.raw.getpeercert()
-    if cert:
-        print("证书信息:")
-        for key, value in cert.items():
-            print(f" {key}: {value}")
+if __name__ == '__main__':
+    spot_price_qh_df = ak.spot_price_qh(symbol='螺纹钢')
+    print(spot_price_qh_df)
