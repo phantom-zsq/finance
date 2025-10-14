e87da128477824083d7156c9e72ab9991984212f
phantom-zsq <1131558536@qq.com>
2025-10-14 12:15:21 +0800

    init

--- Download_Akshare/期货数据/期货交易费用参照表.py
+++ Download_Akshare/期货数据/期货交易费用参照表.py
@@ -1,6 +1,7 @@
 import akshare as ak
 import pandas as pd
 from sqlalchemy import create_engine
+from datetime import date
 
 if __name__ == '__main__':
     # set option of pandas
@@ -10,5 +11,10 @@ if __name__ == '__main__':
     # create mysql engine
     engine = create_engine('mysql+pymysql://root:12345678@localhost:3306/akshare')
     # today
+    current_date = date.today()
+    trade_date = current_date.strftime("%Y%m%d")
+    print(trade_date)
+    # query
     futures_fees_info_df = ak.futures_fees_info()
+    futures_fees_info_df['交易日'] = trade_date
     res = futures_fees_info_df.to_sql('futures_fees_info', engine, index=False, if_exists='append', chunksize=10000)
--- Download_Akshare/预警.sql
+++ Download_Akshare/预警.sql
@@ -1,15 +1,24 @@
+-- 个数统计 --
+select '涨跌停_今日',count(*) from futures_rule where 交易日='20251014'
+union
+select '涨跌停_昨日',count(*) from futures_rule where 交易日='20251013'
+union
+select '保证金_今日',count(*) from futures_fees_info where 交易日='20251014'
+union
+select '保证金_昨日',count(*) from futures_fees_info where 交易日='20251013';
+
 -- 涨跌幅预警 --
 select * from
-(select 交易所,品种,代码,涨跌停板幅度 from futures_rule where 交易日='20251013') a
+(select 交易所,品种,代码,涨跌停板幅度 from futures_rule where 交易日='20251014') a
 inner join
-(select 交易所,品种,代码,涨跌停板幅度 from futures_rule where 交易日='20251010') b
+(select 交易所,品种,代码,涨跌停板幅度 from futures_rule where 交易日='20251013') b
 on a.交易所 = b.交易所 and a.品种 = b.品种 and a.代码 = b.代码
 where a.涨跌停板幅度 != b.涨跌停板幅度;
 
 -- 保证金预警 --
 select * from
-(select 交易所,合约代码,做多保证金率 from futures_fees_info where DATE_FORMAT(STR_TO_DATE(更新时间, '%Y-%m-%d %H:%i:%s'), '%Y%m%d')='20251014') a
+(select 交易所,合约代码,做多保证金率 from futures_fees_info where 交易日='20251014') a
 inner join
-(select 交易所,合约代码,做多保证金率 from futures_fees_info where DATE_FORMAT(STR_TO_DATE(更新时间, '%Y-%m-%d %H:%i:%s'), '%Y%m%d')='20251013') b
+(select 交易所,合约代码,做多保证金率 from futures_fees_info where 交易日='20251013') b
 on a.交易所 = b.交易所 and a.合约代码 = b.合约代码
 where a.做多保证金率 != b.做多保证金率;
